afb96b571c46d4d3f67d986afb1604531551da09
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-12 20:59:26 -0500

    node-db3 resolved empty steps array

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -17,10 +17,13 @@ async function findById(scheme_id) {
     .leftJoin("steps as st", "sc.scheme_id", "=", "st.scheme_id")
     .where("sc.scheme_id", scheme_id)
     .orderBy("st.step_number");
+  console.log(idArray);
 
   const result = idArray.reduce((acc, step) => {
     const { scheme_name, instructions, step_id, step_number } = step;
-    if (acc.steps) {
+    if (!step.step_id) {
+      return { scheme_id, scheme_name, steps: [] };
+    } else if (acc.steps) {
       acc.steps.push({ scheme_id, scheme_name, instructions });
     } else {
       acc = {
@@ -32,12 +35,6 @@ async function findById(scheme_id) {
     return acc;
   }, {});
   return result;
-  // 5B- This is what the result should look like _if there are no steps_ for a `scheme_id`:
-  //   {
-  //     "scheme_id": 7,
-  //     "scheme_name": "Have Fun!",
-  //     "steps": []
-  //   }
 }
 
 function findSteps(scheme_id) {
